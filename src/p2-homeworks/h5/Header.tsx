90d66a92317abdab719ac91bf080caf569469c93
Dmitry Kelek <kelekofficial@gmail.com>
2021-05-02 22:53:37 +0300

    add nawlinks

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
+import S from "./Nav.module.css"
 
 function Header() {
     return (
-        <div>
+        <div className={S.menu_box}>
+            <div className={S.bubble}/>
+            <NavLink to={PATH.PRE_JUNIOR}>
+                <div className={S.pre_junior + " " + S.block}>
+                    Pre Junior
+                </div>
+            </NavLink>
+            <NavLink to={PATH.JUNIOR}>
+                <div className={S.junior + " " + S.block}>
+                    Junior
+                </div>
+            </NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}>
+                <div className={S.junior_plus + " " + S.block}>
+                    Junior +
+                </div>
+            </NavLink>
         </div>
     )
 }
