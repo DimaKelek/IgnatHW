a6648f12a25908da804654a071a7b70972f57055
Dmitry Kelek <kelekofficial@gmail.com>
2021-05-02 21:51:12 +0300

    add page 404 style

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import S from './Error404.module.css'
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
+        <div className={S.main}>
+            <div className={S.message_box}>
+                <div className={S.angle}/>
+                <div className={S.message}>
+                    <div className={S.messageText}>What are you doing here motherfucker?</div>
+                </div>
+            </div>
+            <div className={S.error_box}>
+                <div>404</div>
+                <div>Page not found</div>
+            </div>
 
         </div>
+
     )
 }
 
